0128f4aa8b329aa588708815bb20c6122d741a15
huangsheng <sheng.huang@desaysv.com>
2025-01-10 17:29:06 +0800

    增加二级fragment的模板

--- app/src/main/java/com/llw/myapplicationkotlin/ui/sound/SoundFragment.kt
+++ app/src/main/java/com/llw/myapplicationkotlin/ui/sound/SoundFragment.kt
@@ -2,9 +2,13 @@ package com.llw.myapplicationkotlin.ui.sound
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.desaysv.mvvm.base.fragment.BaseMvvmFragment
 import com.desaysv.mvvm.base.fragment.BaseToolbarFragment
+import com.desaysv.mvvm.ext.onClick
 import com.desaysv.mvvm.log.LogUtil
+import com.llw.myapplicationkotlin.R
 import com.llw.myapplicationkotlin.databinding.FragmentSoundBinding
 import com.llw.myapplicationkotlin.ui.sound.viewmodel.SoundViewModel
 
@@ -14,9 +18,19 @@ import com.llw.myapplicationkotlin.ui.sound.viewmodel.SoundViewModel
  * @Author      : uids0505
  */
 class SoundFragment : BaseMvvmFragment<FragmentSoundBinding, SoundViewModel>(){
-
+    private lateinit var navController: NavController
     override fun initView(view: View, savedInstanceState: Bundle?) {
         LogUtil.d("initView")
+        // 获取 NavController
+        navController = childFragmentManager.findFragmentById(R.id.nav_sound)
+            ?.findNavController() ?: throw IllegalStateException("NavController not found")
+        mBinding?.btnEffectSetting?.onClick {
+            navController.navigate(R.id.soundEffectFragment)
+        }
+
+        mBinding?.btnVolume?.onClick {
+            navController.navigate(R.id.volumeFragment)
+        }
     }
 
 //    override fun setupTitleBar() {
